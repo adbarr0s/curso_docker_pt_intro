445e7d80f9a52e6029832bd5f2c31c6cd4be2bb8
Adriano Barros <adbarr0s@hotmail.com>
2025-02-09 05:15:30 -0600

    update

--- 8_works/1-imagens/app.js
+++ 8_works/1-imagens/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5001
 
 app.get('/' , (req, resp) => {
-resp.send('Ol+a minha imagem')
+resp.send('Ola,  minha imagem nova, correndo na porta '+port)
 });
 
 app.listen(port, () => {
